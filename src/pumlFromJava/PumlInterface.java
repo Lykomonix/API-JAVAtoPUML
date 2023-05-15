43e7bb3709888a120bf0943a744c96f1c4384b73
h.hartz <henri.hartz@etu.unistra.fr>
2023-05-15 14:56:00 +0200

    [*]modification de PumlInterface

--- src/pumlFromJava/PumlInterface.java
+++ src/pumlFromJava/PumlInterface.java
@@ -1,6 +1,8 @@
 package pumlFromJava;
 
 import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
 import java.util.ArrayList;
 
 public class PumlInterface extends PumlElement implements PumlLinkable {
@@ -18,13 +20,55 @@ public class PumlInterface extends PumlElement implements PumlLinkable {
     public String toDCA() {
         StringBuilder builder = new StringBuilder();
 
-        builder.append("class \"<<interface>>\\n " + this.element.getSimpleName()+"\"");
+        builder.append("class \"<<interface>>\\n " + this.element.getSimpleName()+"\" as "+this.element.getSimpleName()+"\n");
 
+        for(PumlLink pumlLink : getLinks())
+        {
+            if(pumlLink.getLinkType() == LinkType.EXTENDS && !pumlLink.getSecondElement().equals("Object"))
+            {
+                builder.append(pumlLink.getFirstElement() + " --|> " + pumlLink.getSecondElement()+"\n");
+            }
+            else
+            {
+                builder.append(pumlLink.getFirstElement() + " ..|> " + pumlLink.getSecondElement()+"\n");
+            }
+        }
         return builder.toString();
     }
 
     @Override
     public ArrayList<PumlLink> getLinks() {
-        return null;
+        ArrayList<PumlLink> Links = new ArrayList<>();
+
+        Links.add(getSuperClass());
+
+        Links.addAll(getInterfaces());
+
+        return Links;
+    }
+
+    private PumlLink getSuperClass() {
+
+        TypeElement typeElement = (TypeElement)this.element;
+
+        String[] superClassFullName = typeElement.getSuperclass().toString().split("\\.");
+
+        PumlLink Link = new PumlLink(typeElement.getSimpleName().toString(),
+                superClassFullName[superClassFullName.length - 1], LinkType.EXTENDS);
+
+        return Link;
+    }
+
+    public ArrayList<PumlLink> getInterfaces(){
+        ArrayList<PumlLink> Links = new ArrayList<PumlLink>();
+        for(TypeMirror typeMirror : ((TypeElement)this.element).getInterfaces())
+        {
+            String[] interfacesFullName = typeMirror.toString().split("\\.");
+
+            PumlLink Link = new PumlLink(this.element.getSimpleName().toString(),
+                    interfacesFullName[interfacesFullName.length - 1], LinkType.IMPLEMENT);
+            Links.add(Link);
+        }
+        return Links;
     }
 }
