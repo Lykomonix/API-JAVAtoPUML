b53f3e9b4f1294807938cd7558abcb125eae8eb3
h.hartz <henri.hartz@etu.unistra.fr>
2023-05-24 16:46:07 +0200

    [+] ajout de commentaire

--- src/pumlFromJava/PumlElement.java
+++ src/pumlFromJava/PumlElement.java
@@ -3,7 +3,7 @@ package pumlFromJava;
 import javax.lang.model.element.Element;
 
 /********************************************************************
- * PumlElement est une classe abstract implémenté par PumlVariable, PumlClass, PumlEnum, PumlInterface, PumlPackage et PumlDiagram
+ * PumlElement est une classe abstract qui permet de forcer la définition des fonctions toDCA et toDCC dans les classes qui l'implémentent
  ********************************************************************/
 public abstract class PumlElement {
     public abstract String toDCA();
